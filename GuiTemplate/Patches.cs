f26eb8d641289636fba57ae903caabb8e9fbd639
bebebeep <lucamcdonald543@gmail.com>
2026-02-15 20:13:04 +0000

    upload src

---
+++ GuiTemplate/Patches.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GorillaNetworking;
+using HarmonyLib;
+
+namespace TDAOI.Patches
+{
+    [HarmonyPatch(typeof(GorillaNetworkJoinTrigger))]
+    [HarmonyPatch("OnBoxTriggered")]
+    public static class GorillaNetworkJoinTriggerPatch
+    {
+        private static void Postfix(GorillaNetworkJoinTrigger __instance)
+        {
+            GorillaNetworkJoinTriggerPatch.LastGorillaNetworkJoinTrigger = __instance;
+        }
+        public static GorillaNetworkJoinTrigger LastGorillaNetworkJoinTrigger;
+    }
+}
